7e4f56df58fe3c7e2aaa8f614d469de909e9768d
Hartleb <g061472@hpc-login02.cm.cluster>
2024-06-23 01:14:08 +0200

    Added .sh file for OMNI Cluster

---
+++ wear.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+
+python main.py --config ./configs/60_frames_30_stride/actionformer_inertial.yaml --seed 1 --eval_type split
